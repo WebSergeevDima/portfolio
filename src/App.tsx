1602bda4e24021876eb5ea32b69ce745e02bb0ff
Sergeev Dima <web.sergeev.dima@gmail.com>
2023-05-23 17:17:50 +0300

    add footer block

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Skills from "./components/Skills/Skills";
 import Works from "./components/Works/Works";
 import Hire from "./components/Hire/Hire";
 import Contacts from "./components/Contacts/Contacts";
+import Footer from "./components/Footer/Footer";
 
 function App() {
     return (
@@ -16,6 +17,7 @@ function App() {
             <Works/>
             <Hire/>
             <Contacts/>
+            <Footer/>
         </div>
     );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import style from './Footer.module.css'
+
+const Footer: React.FC = () => {
+    return (
+        <footer className={style.footer}>
+            <h2 className={style.title}>Дмитрий Сергеев</h2>
+           <div className={style.images}>
+               <div><img src="./images/photo.jpg" className={style.image} alt=""/></div>
+               <div><img src="./images/photo.jpg" className={style.image} alt=""/></div>
+               <div><img src="./images/photo.jpg" className={style.image} alt=""/></div>
+               <div><img src="./images/photo.jpg" className={style.image} alt=""/></div>
+           </div>
+
+            <div className={style.copy}>2023 Все права защищены</div>
+        </footer>
+    )
+}
+
+export default Footer;
